b62962292728dcf46bd9c89fa0b1ba3d235c6a4a
Zain01 <zain.imdad@dynamologic.com>
2019-08-28 18:21:58 +0500

    build push

---
+++ precache-manifest.20531889f31e404baed963ad2894b002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7c8f6316254a8ba0040487a003f609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde44db590f051d94b5e",
+    "url": "/static/css/main.1a3db323.chunk.css"
+  },
+  {
+    "revision": "9786414f954bc858c718",
+    "url": "/static/js/2.963b8ae0.chunk.js"
+  },
+  {
+    "revision": "bde44db590f051d94b5e",
+    "url": "/static/js/main.adef5e66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "affcfe9ea5756e656336f5d6b611aba5",
+    "url": "/static/media/herobis.affcfe9e.png"
+  },
+  {
+    "revision": "56ea9599b2643e3237604c7de57ded9b",
+    "url": "/static/media/heroiqr.56ea9599.png"
+  }
+]);
