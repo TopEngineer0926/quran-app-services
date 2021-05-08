bf96cce3281ee4ba8600e935e6ee783c06ec2511
Top Engineer <top_engineer_0926@outlook.com>
2021-05-08 09:05:11 +0800

    changed the audio files path

--- backup/api/app/Models/Enum.php
+++ backup/api/app/Models/Enum.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Enum extends Model
 {
     //
-    const url_audio = 'https://s3.us-east-2.amazonaws.com/quran.com/verses/';
+    const url_audio = 'https://s3.eu-central-1.amazonaws.com/koranrecitation/verses/';
     const resource_type = [ // resource types for translated_names
         'languages' => 'languages',
         'chapters' => 'chapters',
